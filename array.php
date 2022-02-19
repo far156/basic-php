d260cb97dc311b290db5245d9c92d6d12ef81710
Farzana Akter <67409808+far156@users.noreply.github.com>
2022-02-20 00:00:05 +0600

    Add files via upload

---
+++ array.php
@@ -0,0 +1,24 @@
+<!-- index array -->
+<?php
+$city =array("Dhaka","khulna","rajshahi");
+//array jonno printer _r use kora hoi print jonno
+print_r($city);
+?>
+<!-- associate array -->
+<?php
+$city =array("Dhaka","khulna","rajshahi");
+
+$run=array("sakib"=>55,"asif"=>50);
+
+echo $run["asif"];
+?>
+<!-- multidimention -->
+
+<?php
+$city =array("Dhaka","khulna","rajshahi");
+$students=array(
+    array("asif",45);
+array("asif",45);
+array("asif",45);
+
+?>
---
+++ forelse.php
@@ -0,0 +1,17 @@
+<?php
+$city =array("Dhaka","khulna","rajshahi");
+foreach($city  as $value){
+    echo $value."<br>";
+}
+
+?>
+<!-- unorder list -->
+<?php
+echo"</ul>";
+$city =array("Dhaka","khulna","rajshahi");
+foreach($city  as $value){
+    echo $value."<br>";
+}
+echo"</ul";
+
+?>
